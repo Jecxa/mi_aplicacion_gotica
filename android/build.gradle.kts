5a6fb4587f1beb13670bbcadcc5e3d1c68754b7e
Jecxania Escobedo <jmadaitec@gmail.com>
2025-05-20 20:58:12 -0600

    Actualización con integración Firebase

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,9 @@
+// build.gradle.kts (nivel raíz del proyecto)
+
+plugins {
+    id("com.google.gms.google-services") version "4.4.2" apply false
+}
+
 allprojects {
     repositories {
         google()
@@ -5,14 +11,13 @@ allprojects {
     }
 }
 
+// Reconfigurar los directorios de compilación
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
     project.evaluationDependsOn(":app")
 }
 
